b3a763ff1f25664ac22b83efe173998dfc909834
stanl <stanley.chu@live.com>
2020-03-22 20:00:06 -0400

    AES-Encrypt Decrypt working completely for raw binary data

--- encrypt_auth.go
+++ encrypt_auth.go
@@ -88,22 +88,9 @@ func HMAC_SHA256(MACKeySlice []byte, lineBytes []byte) []byte {
 
 func encrypt(lineBytes []byte, MACKeySlice []byte, encryptionKeySlice []byte) {
 	HMACTag := HMAC_SHA256(MACKeySlice, lineBytes)
-	//messagePrime := make([]byte, len(lineBytes))
-	//copy(messagePrime, lineBytes)
-	//for _, n := range HMACTag {
-	//	x := int64(n)
-	//	binary := strconv.FormatInt(x, 2) + " "
-	//	fmt.Println([]byte(binary))
-	//	fmt.Println(binary)
-	//	messagePrime = append(messagePrime, []byte(binary)...)
-	//}
 
 	messagePrime := append(lineBytes, HMACTag...)
-	//fmt.Println(string(lineBytes))
-	//writeToFile(os.Args[7], messagePrime)
 
-	fmt.Println("MessagePrime: ")
-	fmt.Println(messagePrime)
 	messageDoublePrime := append(messagePrime, CalcPS(messagePrime)...)
 
 	//Init AES Block being used with encryption key given.
@@ -113,39 +100,17 @@ func encrypt(lineBytes []byte, MACKeySlice []byte, encryptionKeySlice []byte) {
 		return
 	}
 	//Initialize AES-CBC
-	//var count int
-	//var initializationVector *big.Int
-	//for count != 16 {
-	//	temp, _ := rand.Int(rand.Reader, big.NewInt(65536))
-	//	for _, x := range temp.Bits() {
-	//		for x != 0 {
-	//			x &= x - 1
-	//			count++
-	//		}
-	//		count++
-	//	}
-	//	temp.Bits()
-	//	initializationVector = temp
-	//}
 	var initializationVector []byte
 	for i:=0; i < 16; i++ {
 		temp, _ := rand.Int(rand.Reader, big.NewInt(256))
 		initializationVector = append(initializationVector, temp.Bytes()...)
 	}
-	//fmt.Printf("LENGTH OF INITVECTOR: %d", len(initializationVector))
-
-	//initializationVector, _ := rand.Int(rand.Reader, 256)
-
-	fmt.Println(initializationVector)
-	//fmt.Printf("%b", initializationVector)
-	fmt.Println(messageDoublePrime)
 	firstPlainBlockBytes := messageDoublePrime[0:16]
 	firstPlainBlock := new(big.Int).SetBytes(firstPlainBlockBytes)
 	XORFirstBlock := new(big.Int).Xor(firstPlainBlock, new(big.Int).SetBytes(initializationVector))
 	XORFirstBlockSlice := XORFirstBlock.Bytes()
 	//Do first round of encryption...destination of ciphertext block is encryptedSlice
 	encryptedSlice := make([]byte, AESBlock.BlockSize())
-	//fmt.Printf("XORBLOCK SLICE SIZE: %d \n\n", len(XORFirstBlockSlice))
 
 	AESBlock.Encrypt(encryptedSlice, XORFirstBlockSlice)
 
@@ -153,7 +118,6 @@ func encrypt(lineBytes []byte, MACKeySlice []byte, encryptionKeySlice []byte) {
 	XORSlice := make([]byte, AESBlock.BlockSize())
 	copy(XORSlice, encryptedSlice)
 
-	fmt.Printf("msgDoublePrime length mod 16: %d\n", len(messageDoublePrime)%16)
 	//Repeat previous AES-CBC procedure for rest of plaintext if needed
 	for i := 16; i <= len(messageDoublePrime); i += 16 {
 		plainBlockBytes := messageDoublePrime[i : i+16]
@@ -162,13 +126,11 @@ func encrypt(lineBytes []byte, MACKeySlice []byte, encryptionKeySlice []byte) {
 		XORBlock := new(big.Int).Xor(plainBlock, bigXORSlice)
 		XORBlockSlice := XORBlock.Bytes()
 
-		//fmt.Println(len(XORBlockSlice))
 		AESBlock.Encrypt(XORSlice, XORBlockSlice)
 		encryptedSlice = append(encryptedSlice, XORSlice...)
 	}
 
 	ciphertext := append(initializationVector, encryptedSlice...)
-	fmt.Println(ciphertext)
 	writeToFile(os.Args[7], ciphertext)
 }
 
@@ -178,17 +140,12 @@ func decrypt(lineBytes []byte, MACKeySlice []byte, encryptionKeySlice []byte) {
 		fmt.Println("error in AES")
 		return
 	}
-	//fmt.Println(lineBytes)
 	//Decrypt first block of ciphertext
 	initVector := lineBytes[0:16]
 	cipherTextBlock := lineBytes[16:32]
 	prePlainText := make([]byte, AESBlock.BlockSize())
 	AESBlock.Decrypt(prePlainText, cipherTextBlock)
 	//XOR initvec and preplaintext for plaintext
-	//messageDoublePrime := make([]byte, AESBlock.BlockSize())
-	//for i, _ := range prePlainText {
-	//	messageDoublePrime[i] = initVector[i] ^ prePlainText[i]
-	//}
 	messageDoublePrimeInt := new(big.Int).Xor(new(big.Int).SetBytes(initVector), new(big.Int).SetBytes(prePlainText))
 	messageDoublePrime := messageDoublePrimeInt.Bytes()
 
@@ -206,11 +163,9 @@ func decrypt(lineBytes []byte, MACKeySlice []byte, encryptionKeySlice []byte) {
 
 		messageDoublePrime = append(messageDoublePrime, temp...)
 	}
-	//fmt.Println(messageDoublePrime)
 
 	//Check that padding is structured correctly
 	n := messageDoublePrime[len(messageDoublePrime)-1]
-	//fmt.Println(n)
 
 	for i := len(messageDoublePrime) - int(n); i < len(messageDoublePrime); i++ {
 		if int(n) != int(messageDoublePrime[i]) {
@@ -222,7 +177,7 @@ func decrypt(lineBytes []byte, MACKeySlice []byte, encryptionKeySlice []byte) {
 	plainText := messagePrime[:len(messagePrime)-32]
 	//Check if MAC tags are matching
 	HMACTag := messagePrime[len(messagePrime)-32:]
-	actualHMACTag := HMAC_SHA256(MACKeySlice, lineBytes)
+	actualHMACTag := HMAC_SHA256(MACKeySlice, plainText)
 	if bytes.Compare(HMACTag, actualHMACTag) != 0 {
 		fmt.Println("INVALID MAC")
 		os.Exit(2)
@@ -230,17 +185,10 @@ func decrypt(lineBytes []byte, MACKeySlice []byte, encryptionKeySlice []byte) {
 		writeToFile(os.Args[7], plainText)
 	}
 
-	//writeToFile(os.Args[7], plainText)
-
 }
 
 func main() {
 	var encryptMode bool
-	//var encryptionKey uint64
-	//var MACKey uint64
-	//var input string
-	//var outFile string
-
 	/** This section of code is to take in command line params and make sure all params are there. */
 	if len(os.Args) < 8 {
 		fmt.Fprintln(os.Stderr, "Please give all params!")
@@ -264,7 +212,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Println(decode)
 	if len(decode) != 32 {
 		fmt.Println("Please give 32 byte key")
 		return
@@ -272,16 +219,6 @@ func main() {
 	MACKeySlice := decode[:16]
 	encryptionKeySlice := decode[16:]
 
-	//fmt.Println(MACKeySlice)
-	//fmt.Println(encryptionKeySlice)
-
-	//TODO: CHECK THIS IS GOOD?!?!?!
-	//encryptionKey = binary.BigEndian.Uint64(encryptionKeySlice)
-	//MACKey = binary.BigEndian.Uint64(MACKeySlice)
-	//fmt.Println(encryptionKey)
-	//fmt.Println(MACKey)
-
-	//var lineBytes []byte
 	lines, err := ioutil.ReadFile(os.Args[5])
 	if err != nil {
 		os.Exit(1)
@@ -294,39 +231,11 @@ func main() {
 		value, _ := strconv.ParseUint(text[i:i+8], 2, 8)
 		lineBytes = append(lineBytes, byte(value))
 	}
-	//var byteLines []byte
-
-	//buf := bytes.NewReader(lines)
-	//err = binary.Read(buf, binary.LittleEndian, &lineBytes)
-	//if err != nil {
-	//	os.Exit(1)
-	//}
-	//
-	//fmt.Println(lines)
 
 	if encryptMode {
 		encrypt(lineBytes, MACKeySlice, encryptionKeySlice)
 	} else {
 		decrypt(lineBytes, MACKeySlice, encryptionKeySlice)
 	}
-
-	//secondHashInput := make([]byte, len(firstHash))
-	//HMACResult := append(outerPadding, []byte(secondHash)...)
-
-	//fmt.Println(len(os.Args), os.Args)
-
-	//for _, x := range os.Args {
-	//	fmt.Println(x)
-	//}
-
-	//var lines []string
-	//scanner := bufio.NewScanner(os.Stdin)
-	//for scanner.Scan() {
-	//	lines = append(lines, scanner.Text())
-	//}
-	//
-	//for _, x := range lines {
-	//	fmt.Printf(x + "\n")
-	//}
 
 }
